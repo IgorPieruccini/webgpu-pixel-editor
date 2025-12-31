7de00f9b1cb4b91654b906237ddd6d334ed7cfa0
Igor Pieruccini <igorpieruccini@gmail.com>
2025-12-31 18:18:50 +0100

    Hide thickness when PAINT_SELECTION is selected

--- src/ui/tools/toolSettings/ToolSettings.tsx
+++ src/ui/tools/toolSettings/ToolSettings.tsx
@@ -1,11 +1,18 @@
+import { ACTIVATE_TOOL } from "../../../editor/constant";
+import { useEditor } from "../../../editor/editortContext";
 import { BrushOpacitySlider } from "./BrushOpacitySlider";
 import { BrushThicknessSlider } from "./BrushThicknessSlider";
 import "./ToolSettings.css";
 
 export const ToolSettings = () => {
+  const project = useEditor();
+
   return (
     <div id="tool-settings">
-      <BrushThicknessSlider />
+      {project?.().activeTool() !== ACTIVATE_TOOL.PAINT_SELECTION && (
+        <BrushThicknessSlider />
+      )}
+
       <BrushOpacitySlider />
     </div>
   );
