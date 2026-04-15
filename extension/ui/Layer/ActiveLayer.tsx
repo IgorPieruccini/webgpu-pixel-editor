17907f7c17191ad6d97b2bafc2769f6204628fda
Igor Pieruccini <igorpieruccini@gmail.com>
2026-04-15 20:35:00 +0200

    Create new domain for the extension

--- extension/ui/Layer/ActiveLayer.tsx
+++ extension/ui/Layer/ActiveLayer.tsx
@@ -3,13 +3,41 @@ import { API, useProject } from "../../../src/lib";
 export const ActiveLayer = () => {
   API.layers();
   const project = useProject();
+  const layers = API.layers();
+
+  const updateLayerOpacity = (e: InputEvent) => {
+    const target = e.target as HTMLInputElement | null;
+    if (!target) {
+      return;
+    }
+
+    const currentLayer = layers().getActive();
+    layers().setOpacity(currentLayer.id, target.valueAsNumber / 100);
+  };
 
   return (
     <div id="layer-info">
+      <div
+        class="opacity-slide"
+        classList={{
+          "preview-canvas-hidden": project.getActiveProject() === null,
+        }}
+      >
+        <label>Layer opacity:</label>
+        <input
+          class="layer-opacity-input"
+          type="range"
+          min={1}
+          max={100}
+          value={layers().getActive().opacity * 100}
+          onInput={updateLayerOpacity}
+        />
+      </div>
+
       <canvas
         id="preview-canvas"
-        width={300}
-        height={300}
+        width={250}
+        height={250}
         classList={{
           // this is a very bad hack, needs ot be fixed
           // when all projects are deleted the preview canvas renders with the last render buffer,
