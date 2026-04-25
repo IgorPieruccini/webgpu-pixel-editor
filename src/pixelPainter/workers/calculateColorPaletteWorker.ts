2e76f4cd35c7db652ced66946773f07f8570cc50
Igor Pieruccini <igorpieruccini@gmail.com>
2026-04-26 01:27:29 +0200

    Update calculateColorPalleteWorker.ts to show right colors in the buffer

--- src/pixelPainter/workers/calculateColorPaletteWorker.ts
+++ src/pixelPainter/workers/calculateColorPaletteWorker.ts
@@ -1,5 +1,3 @@
-import { numberToHex } from "../utils";
-
 export type WorkerRequest = {
   buffers: Array<Uint8Array<ArrayBuffer>>;
 };
@@ -16,16 +14,18 @@ self.onmessage = (event: MessageEvent<WorkerRequest>) => {
 
   const bufferArray = Object.values(buffers);
 
+  const byteToHex = (value: number) => value.toString(16).padStart(2, "0");
+
   for (let i = 0; i < bufferArray.length; i++) {
     const buffer = buffers[i];
     for (let i = 0; i < buffer.length; i += stride) {
       const r = buffer[i];
       const g = buffer[i + 1];
       const b = buffer[i + 2];
-      const a = buffer[i + 3];
-
-      const color = (r << 24) | (g << 16) | (b << 8) | a;
-      const hex = numberToHex(color);
+      const hex = `#${byteToHex(r)}${byteToHex(g)}${byteToHex(b)}`;
+      if (hex === "#000000") {
+        continue;
+      }
       hexColor.add(hex);
     }
   }
